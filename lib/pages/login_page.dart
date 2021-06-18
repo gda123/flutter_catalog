47e145da155abf08cf1d7c132e372f5172fe694f
Gagan Deep Amla <gagandeepamla@Gagans-MacBook-Pro.local>
2021-06-18 12:06:12 +0530

    Four Commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({ Key? key }) : super(key: key);
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text('Login Page'),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/login.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+            child: Text('LOGIN', style: TextStyle(fontStyle: FontStyle.italic),),
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 21,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username"
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                    onPressed: (){
+                      print("Hi Ginfomatics Web & Software Solutions");
+                    },
+                    child: Text('Log In'))
+              ],
+            ),
+          ),
+
+
+        ],
       ),
     );
   }
